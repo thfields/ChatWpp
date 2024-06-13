cf55f5c5844a53e506eab39ade9859098033a2ad
Thiago Campos <thiagoabcfc@hotmail.com>
2024-06-13 15:24:34 -0300

    inserindo imagens

--- src/components/Chat/ZapChat.jsx
+++ src/components/Chat/ZapChat.jsx
@@ -1,14 +1,22 @@
-// src/components/WhatsAppLayout.jsx
+// src/components/ZapChat.jsx
 
 import { useState } from 'react';
 import './ZapChat.css';
 
+// Mapeamento de nomes de contatos para URLs das imagens de perfil
+const contactProfileImages = {
+  "Vitor": "/src/assets/vitor.PNG",
+  "(84) 99617-1333": "/src/assets/sem-foto.png",
+  "Thiago": "/src/assets/thiago.PNG"
+};
+
 const ZapChat = () => {
-  const initialContacts = ["Vitor", "Daniel", "Thiago"];
+  const initialContacts = ["Vitor", "(84) 99617-1333", "Thiago"];
 
   const [selectedContact, setSelectedContact] = useState(null);
   const [messageInput, setMessageInput] = useState('');
   const [contactsMessages, setContactsMessages] = useState({});
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleContactClick = (contact) => {
     setSelectedContact(contact);
@@ -45,21 +53,44 @@ const ZapChat = () => {
     }
   };
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredContacts = initialContacts.filter(contact =>
+    contact.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="whatsapp-layout">
       <div className="sidebar">
         <div className="sidebar-header">
           <h2>Contatos</h2>
+          <input
+            type="text"
+            placeholder="Pesquisar contatos"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="search-input"
+          />
         </div>
         <div className="contact-list">
-          {initialContacts.map((contact, index) => (
-            <div key={index} className="contact" onClick={() => handleContactClick(contact)}>{contact}</div>
+          {filteredContacts.map((contact, index) => (
+            <div key={index} className="contact" onClick={() => handleContactClick(contact)}>
+              <img src={contactProfileImages[contact]} alt={`${contact}'s Profile`} className="profile-img" />
+              <span className="contact-name">{contact}</span>
+            </div>
           ))}
         </div>
       </div>
       <div className="chat-area">
         <div className="chat-header">
-          <h2>{selectedContact ? `${selectedContact}` : 'Selecione um contato'}</h2>
+          {selectedContact && (
+            <div className="contact-info">
+              <img src={contactProfileImages[selectedContact]} alt={`${selectedContact}'s Profile`} className="profile-img" />
+              <h2 className="contact-name">{selectedContact}</h2>
+            </div>
+          )}
         </div>
         <div className="chat-messages">
           {selectedContact && contactsMessages[selectedContact] && contactsMessages[selectedContact].map((msg, index) => (
