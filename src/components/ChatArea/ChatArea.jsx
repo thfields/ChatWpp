16b7752b39fc76037413ab0ba9070cb02be754ed
Thiago Campos <thiagoabcfc@hotmail.com>
2024-06-18 19:43:56 -0300

    feat: search chat ok

--- src/components/ChatArea/ChatArea.jsx
+++ src/components/ChatArea/ChatArea.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { MagnifyingGlass, DotsThreeOutlineVertical, DownloadSimple } from "@phosphor-icons/react";
 import InicialScreen from '../../pages/InicialSreen/InicialScreen';
 
 const ChatArea = ({ selectedContact, contactProfileImages, contactsMessages }) => {
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const toggleSearchBar = () => {
+    setSearchVisible(!searchVisible);
+    setSearchTerm(''); // Limpar o termo de pesquisa quando a barra de pesquisa for aberta/fechada
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const highlightText = (text, highlight) => {
+    if (!highlight) return text;
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return parts.map((part, index) => (
+      part.toLowerCase() === highlight.toLowerCase() ? <mark key={index}>{part}</mark> : part
+    ));
+  };
+
+  const filteredMessages = contactsMessages[selectedContact] ? contactsMessages[selectedContact].filter(msg => 
+    msg.content.toLowerCase().includes(searchTerm.toLowerCase())
+  ) : [];
+
   return (
     <div className="chat-area">
       {!selectedContact && <InicialScreen />}
@@ -13,13 +38,25 @@ const ChatArea = ({ selectedContact, contactProfileImages, contactsMessages }) =
               <img src={contactProfileImages[selectedContact]} alt={`${selectedContact}'s Profile`} className="profile-img" />
               <h2 className="contact-name">{selectedContact}</h2>
               <div className='icon-chat'>
-                <MagnifyingGlass size={32} />
-                <DotsThreeOutlineVertical size={22} />
+                <button onClick={toggleSearchBar}><MagnifyingGlass size={32} /></button>
+                
               </div>
+              
             </div>
+            {searchVisible && (
+              <div className="chat-search-bar">
+                <input
+                  type="text"
+                  placeholder="Pesquisar no chat..."
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                />
+              </div>
+            )}
+          <DotsThreeOutlineVertical size={22} />
           </div>
           <div className="chat-messages">
-            {contactsMessages[selectedContact] && contactsMessages[selectedContact].map((msg, index) => (
+            {filteredMessages.map((msg, index) => (
               <div
                 key={index}
                 className={`message ${msg.sender === 'Me' ? 'sent' : 'received'}`}
@@ -32,7 +69,7 @@ const ChatArea = ({ selectedContact, contactProfileImages, contactsMessages }) =
                       </a>
                     </div>
                   ) : (
-                    <span>{msg.content}</span>
+                    <span>{highlightText(msg.content, searchTerm)}</span>
                   )}
                 </div>
               </div>
